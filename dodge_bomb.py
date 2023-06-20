79fe4bf3fa440d713fc7cc28c841217d84a7dbd1
Irie <c0a220206d@edu.teu.ac.jp>
2023-06-20 14:24:56 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -21,6 +21,7 @@ def main():
     # 爆弾Surface(bd_rct)の爆弾Rect(bd_rct)を抽出する
     bd_rct.center = x, y  
     # 爆弾rectの中心座標を乱数で指定する
+    vx, vy = +5, +5  # 練習２
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -30,6 +31,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx,vy)  # 練習２
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
