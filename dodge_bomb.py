457032d85185ac43f9ddddb12440f2e06970947f
Irie <c0a220206d@edu.teu.ac.jp>
2023-06-20 14:18:42 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,4 @@
-import sys
+import sys, random
 import pygame as pg
 
 
@@ -11,6 +11,16 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bd_img = pg.Surface((20,20))  # 練習１
+    bd_img.set_colorkey((0, 0, 0))  
+    # 爆弾Surfaceの黒い部分を透明にする
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    bd_rct=bd_img.get_rect()  
+    # 爆弾Surface(bd_rct)の爆弾Rect(bd_rct)を抽出する
+    bd_rct.center = x, y  
+    # 爆弾rectの中心座標を乱数で指定する
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +30,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
